07a13ff86b738cc3172a2789022b8002586d620f
andrew644 <andrew644@users.noreply.github.com>
2025-05-30 00:22:15 -0400

    Show white dots

--- src/project2/project2.zig
+++ src/project2/project2.zig
@@ -6,11 +6,31 @@ const glfw = @cImport({
 const gl = @cImport({
     @cInclude("GL/gl.h");
 });
+const glad = @cImport({
+    @cInclude("include/glad/glad.h");
+});
 const ass = @cImport({
     @cInclude("assimp/cimport.h");
     @cInclude("assimp/scene.h");
     @cInclude("assimp/postprocess.h");
 });
+
+const vertex_shader_src =
+    \\#version 330 core
+    \\layout (location = 0) in vec3 aPos;
+    \\void main() {
+    \\    gl_Position = vec4(aPos, 1.0);
+    \\    gl_PointSize = 10.0;
+    \\}
+;
+
+const fragment_shader_src =
+    \\#version 330 core
+    \\out vec4 FragColor;
+    \\void main() {
+    \\    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    \\}
+;
 
 pub fn project2() !void {
     const filename = "assets/teapot.obj";
@@ -20,15 +40,14 @@ pub fn project2() !void {
         return;
     }
     defer ass.aiReleaseImport(scene);
-    std.debug.print("Meshes: {d}\n", .{scene.*.mNumMeshes});
-    for (scene.*.mMeshes[0..scene.*.mNumMeshes]) |mesh| {
-        std.debug.print("Mesh has {d} vertices\n", .{mesh.*.mNumVertices});
-        for (mesh.*.mVertices[0..mesh.*.mNumVertices], 0..) |vertex, i| {
-            const x = vertex.x;
-            const y = vertex.y;
-            const z = vertex.z;
-            std.debug.print("Vertex {d}: ({d}, {d}, {d})\n", .{ i, x, y, z });
-        }
+    const mesh = scene.*.mMeshes[0];
+    const num_verticies = mesh.*.mNumVertices;
+    const vertices = mesh.*.mVertices[0..num_verticies];
+    const float_vertices = try std.heap.c_allocator.alloc(f32, num_verticies * 3);
+    for (vertices, 0..) |v, i| {
+        float_vertices[i * 3 + 0] = v.x;
+        float_vertices[i * 3 + 1] = v.y;
+        float_vertices[i * 3 + 2] = v.z;
     }
 
     if (glfw.glfwInit() == glfw.GLFW_FALSE) {
@@ -47,14 +66,58 @@ pub fn project2() !void {
     defer glfw.glfwTerminate();
     defer glfw.glfwDestroyWindow(window);
 
+    if (glad.gladLoadGLLoader(@ptrCast(&glfw.glfwGetProcAddress)) == 0) {
+        std.debug.print("GLAD failed.\n", .{});
+        return error.GLADInitFailed;
+    }
+    var vao: gl.GLuint = undefined;
+    if (glad.glad_glGenVertexArrays) |f| f(1, &vao);
+    glad.glBindVertexArray(vao);
+
+    var vbo: gl.GLuint = undefined;
+    glad.glGenBuffers(1, &vbo);
+    glad.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo);
+    glad.glBufferData(gl.GL_ARRAY_BUFFER, num_verticies * @sizeOf(f32), @ptrCast(&float_vertices), gl.GL_STATIC_DRAW);
+
+    glad.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 3 * @sizeOf(f32), null);
+    glad.glEnableVertexAttribArray(0);
+
+    //Shader
+    const vertex_shader = try createShader(gl.GL_VERTEX_SHADER, vertex_shader_src);
+    const fragment_shader = try createShader(gl.GL_FRAGMENT_SHADER, fragment_shader_src);
+    const shader_program = glad.glCreateProgram();
+    glad.glAttachShader(shader_program, vertex_shader);
+    glad.glAttachShader(shader_program, fragment_shader);
+    glad.glLinkProgram(shader_program);
+    glad.glDeleteShader(vertex_shader);
+    glad.glDeleteShader(fragment_shader);
+
     while (glfw.glfwWindowShouldClose(window) != glfw.GLFW_TRUE) {
         if (glfw.glfwGetKey(window, glfw.GLFW_KEY_ESCAPE) == glfw.GLFW_PRESS) {
             glfw.glfwSetWindowShouldClose(window, glfw.GLFW_TRUE);
         }
 
-        gl.glClearColor(0.2, 0.3, 0.3, 1.0);
+        gl.glClearColor(0.1, 0.1, 0.1, 1.0);
         gl.glClear(gl.GL_COLOR_BUFFER_BIT);
+
+        glad.glUseProgram(shader_program);
+        glad.glBindVertexArray(vao);
+        glad.glDrawArrays(gl.GL_POINTS, 0, @intCast(num_verticies));
+
         glfw.glfwSwapBuffers(window);
         glfw.glfwPollEvents();
     }
+}
+
+fn createShader(kind: gl.GLenum, src: []const u8) !gl.GLuint {
+    const id = glad.glCreateShader(kind);
+    const srcs = [_][*c]const u8{src.ptr};
+    glad.glShaderSource(id, 1, &srcs[0], null);
+    glad.glCompileShader(id);
+
+    var success: gl.GLint = 0;
+    glad.glGetShaderiv(id, glad.GL_COMPILE_STATUS, &success);
+    if (success == 0) return error.ShaderCompileError;
+
+    return id;
 }
